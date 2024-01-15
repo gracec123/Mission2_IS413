844be52efdd4a51ec44d89fb9e2de72a4e9387cc
gracec123 <gracecwyu@gmail.com>
2024-01-15 01:12:13 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Mission2_IS413;
+using System;
+using System.Xml.Linq;
+
+
+// First Class called Program to allow user input number of times of rolling two 6-sided dice.
+internal class Program
+{
+    private static void Main()
+    {
+        // Set variable rolls is integer
+        int rolls = 0;
+
+        // Show welcome message
+        System.Console.WriteLine("Welcome to the dice throwing simulator!" + "\n");
+
+        // Allows User to Input Number of Rolls
+        System.Console.WriteLine("How many dice rolls would you like to simulate? ");
+        rolls = int.Parse(System.Console.ReadLine());
+
+        // Program Passes Number of Rolls to Result Class
+        int[] resultArray = Result.DiceResult(rolls);
+
+        // This is the decription of the rolling dices game and result
+        System.Console.WriteLine("\n" + "DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + rolls + "." + "\n");
+
+        // Loop through possible sums of two dice (2 to 12)
+        for (int i = 2; i <= 12; i++)
+        {
+            // Calculate the percentage of occurrences for the current sum
+            int percentage = (resultArray[i] * 100) / rolls;
+
+            // Display the histogram bar for the current sum
+            System.Console.Write(i + ": " + new string('*', (int)percentage) + "\n");
+        }
+
+        // Closing Message 
+        System.Console.WriteLine("\n" + "Thank you for using the dice throwing simulator. Goodbye!");
+
+    }
+
+
+
+
+
+}
---
+++ Result.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2_IS413
+{
+    // This is sceond class called Result
+    internal class Result
+    {
+        // A method called DiceResult and n is the number of rolls has been passed from the first class
+        public static int[] DiceResult(int n)
+
+
+        {
+            //  creating an instance of the Random class 
+            Random random = new Random();
+
+            // Initializes an integer array named array, The array has a length of 13 (0 -12)
+            int[] array = new int[13];
+
+            // Base on the number of n to run this for loop
+            for (int i = 0; i < n; i++)
+            {
+                // Generate a random integer for Dice 1 (1-6)
+                int dice1 = random.Next(1, 7);
+
+                // Generate a random integer for Dice 2 (1-6)
+                int dice2 = random.Next(1, 7);
+
+                // Add the total numbers of 2 dices
+                int total = dice1 + dice2;
+
+                // Count the occurrences of each total and increment the corresponding array index
+                array[total]++;
+
+            }
+
+            return array;
+
+
+        }
+    }
+}
